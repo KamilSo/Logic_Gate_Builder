df9a8ed63f9ce896bd1bfd65c880e0c95cdda452
ksobo <k.sobolewski2@gmail.com>
2024-01-21 16:48:56 +0000

    Logic Gate Builder

--- practise.py
+++
@@ -1,6 +0,0 @@
-txt = 'pinkfairyarmadillo'
-index,word_list = ([n for n in range(len(txt)) if txt[n] in ['a','e','i','o','u']],[letter for letter in txt])
-for int_index,i in enumerate(index):
-	word_list[index[0]], word_list[index[-1]] = word_list[index[-1]],word_list[index[0]]
-	del index[0], index[-1]
-print(*word_list)
